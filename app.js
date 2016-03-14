f0aa4045df06f154c162827d49dcaf2591d66af0
Andrew Stelmach <Andrew.Stelmach@sky.uk>
2016-03-14 14:01:32 +0000

    submitted form fields printing to console

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.get('/register', function (req, res) {
-  res.sendFile('./views/registration.html');
+  res.sendfile('./views/registration.html');
 });
 
 app.post('/register', function (req, res) {
-  console.log(req)
+  console.log(req.body)
 });
 
 app.listen(3000, function () {
