14f224dffc72321f19cf9628ea248bf056e63e19
Andrew Stelmach <Andrew.Stelmach@sky.uk>
2016-03-14 13:49:32 +0000

    console logging entire request from submitted form

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));
+
 app.get('/register', function (req, res) {
-  res.sendfile('./views/registration.html');
+  res.sendFile('./views/registration.html');
+});
+
+app.post('/register', function (req, res) {
+  console.log(req)
 });
 
 app.listen(3000, function () {
