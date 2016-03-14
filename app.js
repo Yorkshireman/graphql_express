5929c506aae0a79d9872d13fdc6fd4d74ff1b8de
Andrew Stelmach <Andrew.Stelmach@sky.uk>
2016-03-14 12:08:35 +0000

    GraphQL Registration page displaying at /register with no template engine - just serving a single static html file

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/register', function (req, res) {
-  res.send('GraphQL Registration');
+  res.sendfile('./views/registration.html');
 });
 
 app.listen(3000, function () {
